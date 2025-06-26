aea33a51e23d5b4b172d4a995f2d559a26c5f062
Mikal <mikalpaudel58@gmail.com>
2025-06-26 15:43:52 +0545

    Refactor component imports and restructure SearchForm and SearchFormReset for improved functionality; add Button component with variants.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,24 @@
-import SearchForm from "../components/SearchForm";
+import SearchForm from "../../components/SearchForm";
 
-export default function Home() {
+export default async function Home({
+  searchParams
+}: {
+  searchParams: Promise<{ query?: string }>
+}) {
+  const query = (await searchParams).query;
   return (
     <>
-    <section className="pink_container">
-      <h1 className="heading">
-        Pitch Your Startup, <br />
-        Get Funded!
-      </h1>
-      <p className="subheading !max-w-3xl">
-        Join our community of entrepreneurs and investors to turn your ideas into reality.
-      </p>
-      <SearchForm />
-    </section>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Get Funded!
+        </h1>
+        <p className="subheading !max-w-3xl">
+          Join our community of entrepreneurs and investors to turn your ideas
+          into reality.
+        </p>
+        <SearchForm query={query} />
+      </section>
     </>
   );
 }
--- components/SearchFormReset.tsx
+++ components/SearchFormReset.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { X } from "lucide-react";
 import Link from "next/link";
 
 const SearchFormReset = () => {
@@ -10,7 +11,7 @@ const SearchFormReset = () => {
   return (
     <button type="reset" onClick={reset}>
       <Link href="/" className="search-btn text-white">
-      X
+        <X className="size-5" />
       </Link>
     </button>
   );
