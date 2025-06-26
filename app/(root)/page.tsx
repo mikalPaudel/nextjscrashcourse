f43c9a8306e5c36b387599cb4e98c963367feaca
Mikal <mikalpaudel58@gmail.com>
2025-06-26 14:34:06 +0545

    Update homepage content to promote startup pitching and funding opportunities

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
     <>
-      <h1 className="heading">Home Page</h1>
+    <section className="pink_container">
+      <h1 className="heading">
+        Pitch Your Startup, <br />
+        Get Funded!
+      </h1>
+      <p className="subheading !max-w-3xl">
+        Join our community of entrepreneurs and investors to turn your ideas into reality.
+      </p>
+      
+    </section>
     </>
   );
 }
