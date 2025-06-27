ae02af53f4dc28730fa91f4ebaf1f5f8cef9ab97
Mikal <mikalpaudel58@gmail.com>
2025-06-27 18:12:39 +0545

    feat: add author schema type and integrate into Sanity structure

---
+++ sanity/schemaTypes/author.ts
@@ -0,0 +1,30 @@
+import { defineField, defineType } from "sanity";
+import { UserIcon } from "lucide-react";
+
+export const author = defineType({
+    name: 'author',
+    title: 'Author',
+    type: 'document',
+    icon: UserIcon,
+    fields: [
+        defineField({
+            name: 'id',
+            type: 'number',
+        }),
+        defineField(
+        {
+            name: 'name',
+            type: 'string',
+        }),
+        defineField(
+        {
+            name: 'username',
+            type: 'string',
+        }),
+        defineField(
+        {
+            name: 'email',
+            type: 'string',
+        })
+    ],
+})
